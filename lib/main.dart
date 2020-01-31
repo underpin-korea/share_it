9f255d99b3f6c1980993d90b04848ddf6384d15e
iwin2471 <iwin12372@gmail.com>
2020-01-31 23:23:11 +0900

    start project

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(accentColor: Colors.lightBlue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(""),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        bottomNavigationBar: BottomAppBar(),
+      ),
+    );
+  }
+}
